fbfdf1245046ccb491ac73fb9f3c393d88bb4b35
syouei12 <syouei1218@gmail.com>
2023-04-04 11:07:13 +0900

    calendarを作成

--- src/pages/player/report.tsx
+++ src/pages/player/report.tsx
@@ -7,14 +7,51 @@ import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import { width } from '@mui/system';
 
 //sxでcssをあてる
-export default function Home() {
+export default function Report() {
   const [value, setValue] = React.useState<number | null>(0);
 
   return (
     <>
     <Container maxWidth="md" sx={{p:4}}>
+      <Box
+      sx={{
+        display:'flex',
+        width:800,
+        mb:2
+      }}
+      >
+      <TextField
+          id="outlined-multiline-static"
+          label="場所"
+          fullWidth
+          defaultValue=""
+          sx={{mx:1}}
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="天気"
+          fullWidth
+          defaultValue=""
+          sx={{mx:1}}
+        />
+      </Box>
+      <Box
+      sx={{
+        display:'flex',
+        width:800
+      }}
+      >
+        <TextField
+          id="outlined-multiline-static"
+          label="今日の目標"
+          fullWidth
+          defaultValue=""
+          sx={{mx:1}}
+        />
+      </Box>
     <Box
       sx={{
         '& > legend': { mt: 2 },
@@ -35,7 +72,10 @@ export default function Home() {
       component="form"
       noValidate
       autoComplete="off"
-      sx={{width:800}}
+      sx={{
+        width:800,
+        mb:2
+      }}
     >
         <TextField
           id="outlined-multiline-static"
@@ -44,9 +84,9 @@ export default function Home() {
           rows={6}
           fullWidth
           defaultValue=""
+          sx={{mb:3}}
         />
-        <br/>
-        <br/>
+
 
         <TextField
           id="outlined-multiline-static"
@@ -56,10 +96,17 @@ export default function Home() {
           fullWidth
           defaultValue=""
         />
-          <Button variant="outlined" sx={{m:10}}>下書き保存</Button>
-          <Button variant="outlined"  href="#outlined-buttons">
-        送信
-      </Button>
+      </Box>
+
+      <Box
+      sx={{
+        display:'flex',
+        justifyContent:'center',
+        width:800,
+      }}>
+      <Button variant="outlined" size="small" sx={{mx:2}}>下書き保存</Button>
+          <Button variant="contained"  sx={{mx:2}}  href="#outlined-buttons">送信</Button>
+
       </Box>
       </Container>
     </>
